06c45b63c138e664a8df7859b6c029fb3217b1f1
Kendall Kant <kantkendall@gmail.com>
2021-11-11 11:47:10 -0500

    Added, app & config toml, init.sh, changed chain Id

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/cosmos/gaia/v6
 go 1.17
 
 require (
-	github.com/Electronic-Signatures-Industries/ancon-protocol v0.2.2
+	github.com/Electronic-Signatures-Industries/ancon-protocol v0.0.0-20211109232705-168db00119fa
 	github.com/cosmos/cosmos-sdk v0.44.3
 	github.com/cosmos/ibc-go/v2 v2.0.0
 	github.com/gorilla/mux v1.8.0
@@ -36,7 +36,6 @@ require (
 	github.com/confio/ics23/go v0.6.6 // indirect
 	github.com/cosmos/go-bip39 v1.0.0 // indirect
 	github.com/cosmos/iavl v0.17.1 // indirect
-	github.com/cosmos/ibc-go v1.2.3 // indirect
 	github.com/cosmos/ledger-cosmos-go v0.11.1 // indirect
 	github.com/cosmos/ledger-go v0.9.2 // indirect
 	github.com/danieljoos/wincred v1.0.2 // indirect
---
+++ init.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+KEY="mykey"
+CHAINID="cosmoshub-testnet"
+MONIKER="anconprotocol"
+KEYRING="test"
+# KEYALGO="eth_secp256k1"
+LOGLEVEL="info"
+# to trace evm
+# TRACE="--trace"
+MNEMONIC="soda comic bachelor scheme absent embrace case toddler medal scrub obtain glad"
+MNEMONICA="lend lock kit kiss walnut flower expect text upset nut arrive hub waste stairs climb neither must crowd harvest network wife lizard shiver obtain"
+MNEMONICB="concert negative cry purse shed chest daughter knock success august axis capable tomato apple seven define gain allow carbon science confirm portion mountain combine"
+# validate dependencies are installed
+command -v jq > /dev/null 2>&1 || { echo >&2 "jq not installed. More info: https://stedolan.github.io/jq/download/"; exit 1; }
+
+# remove existing daemon and client
+rm -rf ~/.gaia*
+
+~/go/bin/gaiad config keyring-backend $KEYRING
+~/go/bin/gaiad config chain-id "cosmoshub-testnet"
+
+# if $KEY exists it should be deleted
+(echo $MNEMONICA) | ~/go/bin/gaiad keys add $KEY --keyring-backend $KEYRING --recover
+#(echo $MNEMONICA)| ~/go/bin/gaiad keys add alice --keyring-backend $KEYRING --algo $KEYALGO --recover
+#(echo $MNEMONICB)| ~/go/bin/gaiad keys add bob --keyring-backend $KEYRING --algo $KEYALGO --recover
+
+# Set moniker and chain-id for Ethermint (Moniker can be anything, chain-id must be an integer)
+~/go/bin/gaiad init $MONIKER --chain-id $CHAINID
+
+# Change parameter token denominations to aphoton
+cat $HOME/.gaia/config/genesis.json | jq '.app_state["staking"]["params"]["bond_denom"]="uatom"' > $HOME/.gaia/config/tmp_genesis.json && mv $HOME/.gaia/config/tmp_genesis.json $HOME/.gaia/config/genesis.json
+cat $HOME/.gaia/config/genesis.json | jq '.app_state["crisis"]["constant_fee"]["denom"]="uatom"' > $HOME/.gaia/config/tmp_genesis.json && mv $HOME/.gaia/config/tmp_genesis.json $HOME/.gaia/config/genesis.json
+cat $HOME/.gaia/config/genesis.json | jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["denom"]="uatom"' > $HOME/.gaia/config/tmp_genesis.json && mv $HOME/.gaia/config/tmp_genesis.json $HOME/.gaia/config/genesis.json
+cat $HOME/.gaia/config/genesis.json | jq '.app_state["mint"]["params"]["mint_denom"]="uatom"' > $HOME/.gaia/config/tmp_genesis.json && mv $HOME/.gaia/config/tmp_genesis.json $HOME/.gaia/config/genesis.json
+
+# increase block time (?)
+# cat $HOME/.gaia/config/genesis.json | jq '.consensus_params["block"]["time_iota_ms"]="1000"' > $HOME/.gaia/config/tmp_genesis.json && mv $HOME/.gaia/config/tmp_genesis.json $HOME/.gaia/config/genesis.json
+
+# Set gas limit in genesis
+# cat $HOME/.gaia/config/genesis.json | jq '.consensus_params["block"]["max_gas"]="10000000"' > $HOME/.gaia/config/tmp_genesis.json && mv $HOME/.gaia/config/tmp_genesis.json $HOME/.gaia/config/genesis.json
+
+
+# Allocate genesis accounts (cosmos formatted addresses)
+# ~/go/bin/gaiad add-genesis-account evmos1plc66geee3un5377d0ltyphyvrpdhsez9l7jr2 100000000000000000000000000aphoton --keyring-backend $KEYRING  --home ~/.gaia
+~/go/bin/gaiad add-genesis-account $KEY 100000000000000000000000000uatom --keyring-backend $KEYRING
+# Sign genesis transaction
+~/go/bin/gaiad gentx $KEY 1000000000000000000000uatom  --keyring-backend $KEYRING --chain-id $CHAINID
+
+# Collect genesis tx
+~/go/bin/gaiad collect-gentxs
+
+# Run this to ensure everything worked and that the genesis file is setup correctly
+~/go/bin/gaiad validate-genesis
+
+if [[ $1 == "pending" ]]; then
+  echo "pending mode is on, please wait for the first block committed."
+fi
+
+cp app.toml $HOME/.gaia/config/app.toml
+
+cp config.toml $HOME/.gaia/config/config.toml
+
+
+#~/go/bin/gaiad keys show alice | echo
+#~/go/bin/gaiad keys show bob | echo
+
+
+# Start the node (remove the --pruning=nothing flag if historical queries are not needed)
+~/go/bin/gaiad start --log_level $LOGLEVEL
+
+#~/go/bin/gaiad tx bank send ethm1jrclh4kgf3467e9aueudn9fflaz04mftahgun3 ancon1x23pcxakulpq74r7jv948kk90apv6f0kpzgp83 10000aphoton
+#~/go/bin/gaiad tx bank send ethm1jrclh4kgf3467e9aueudn9fflaz04mftahgun3 ethm1yf7eqee4l9hen2g3q799j92k638e98lfq84635 10000aphoton
